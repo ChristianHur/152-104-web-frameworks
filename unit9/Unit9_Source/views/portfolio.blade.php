7c7b09b55a0335b8546fbeedc181fe65b50879f1
Christian Hur <chrisvhur@gmail.com>
2022-07-10 00:07:04 -0500

    Add files via upload

---
+++ unit9/Unit9_Source/views/portfolio.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title', $title)
+
+@section('content')
+        <main>
+            <h1 id="title">{{ $title }}</h1>
+            
+        </main>    
+@endsection
