ae63899c7c106f1e306d2bb4813c11c45f112d11
Christian Hur <chrisvhur@gmail.com>
2022-07-10 00:06:27 -0500

    Add files via upload

---
+++ unit9/Unit9_Source/views/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.master')
+
+@section('content')
+
+<main>
+    <h1 id="title">{{ $title }}</h1>
+    <section>
+        <h1>What's Ninja?</h1>
+        <article>
+            <p>Those black suits. Those weapons. Martial arts. The image of the ninja is rooted more in fiction
+                than fact. Everything you think you know is probably more "wrong" than "right".</p>
+
+            <p><i>This article was originally published on August 7, 2012.</i></p>
+
+            <p>Ninja most certainly did exist in Japan, but they were more about information—and
+                disinformation—than just assassination. Yes, there were ninja assassins, but as Ninja Attack
+                author and game localizer Matt Alt points out, what percentage of CIA employees are trained
+                assassins?</p>
+            <cite> <small> Source: <a
+                href="https://kotaku.com/all-you-know-about-ninja-is-probably-wrong-5932403">https://kotaku.com/all-you-know-about-ninja-is-probably-wrong-5932403</a> </small></cite>
+        </article>
+    </section>
+</main>
+@endsection
