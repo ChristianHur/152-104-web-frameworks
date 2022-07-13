b3a53e9e7000d5cfd9c511498315ff07e19b54e7
Christian Hur <chrisvhur@gmail.com>
2022-07-12 20:52:25 -0500

    Create data.php

---
+++ unit9/Unit9_Source/datafiles/data.php
@@ -0,0 +1,26 @@
+const projectData = [
+    [
+        'id' => 123,
+        'title' => "Personal Site",
+        'description' => 'My person web site using Node.js/Express.js.',
+        'url' => 'http://my-personal-site.com'
+    ],
+    [
+        'id' => 345,
+        'title' => "Hur and Associates, LLC.",
+        'description' => 'My future business web site built using PHP and MySQL.',
+        'url' => 'http://hurandassociates.com'
+    ],
+    [
+        'id' => 678,
+        'title' => "Pokemon Game",
+        'description' => 'Web-based Pokemon game built using Angular.',
+        'url' => 'http://go-pokemon-game.com'
+    ],
+    [
+        'id' => 910,
+        'title' => "Auction Site",
+        'description' => 'A client\'s auction site built with Django, Python, and MongoDB.',
+        'url' => 'http://auction-house.com'
+    ],
+];
